d595d9e419f5234e0c0292101e0fc9c2066164ef
Maham Shahid <161121868+MAHAM42@users.noreply.github.com>
2024-04-25 10:00:10 -0700

    44-Assignment
    TypeScript 45-Assignment of Q44 with Node.js

---
+++ main44.ts
@@ -0,0 +1,26 @@
+// ASSIGNMENT NO 44:
+
+/*
+Sandwiches: Write a function that accepts a array of items a person wants on a sandwich.
+The function should have one parameter that collects as many items as the function call provides, 
+and it should print a summary of the sandwich that is being ordered. Call the function three times, 
+using a different number of arguments each time.
+*/
+
+console.log("ASSIGNMENT NO 44:");
+
+// Define a function with a rest parameter that accept items arugments representing our sandwich:
+function makeSandwich(...items: string[]) {
+    console.log("\n Making a sandwich with the following items: \n");
+
+    items.forEach(singleItem => console.log("-" + singleItem));
+
+    console.log("\n Now Enjoy Sandwich");
+}
+
+// Call the function 3 items with 3 different number of arguments:
+makeSandwich("Chicken", "Cheese", "Mayo", "Egg");
+
+makeSandwich("Bread", "Butter");
+
+makeSandwich("Bread", "Butter", "Mayo", "Egg", "Cheese", "Chicken", "Lettuce", "Tomato");
